5275663c3898aef461fc126bc9f8f48012161a98
danilodesousa <danilod2.sousa@gmail.com>
2019-12-26 00:25:10 -0300

    Geração do token de sessão

--- src/database/index.js
+++ src/database/index.js
@@ -1,20 +1,19 @@
 import Sequelize from "sequelize";
-import mongoose from "mongoose";
 import User from "../app/models/User";
 import File from "../app/models/File";
 import Enrollment from "../app/models/Enrollment";
 import Checkin from "../app/models/Checkin";
 import Student from "../app/models/Student";
 import Plan from "../app/models/Plan";
+import Helper from "../app/models/Helper";
 
 import databaseConfig from "../config/database";
 
-const models = [User, File, Enrollment, Checkin, Student, Plan];
+const models = [User, File, Enrollment, Checkin, Student, Plan, Helper];
 
 class Database {
   constructor() {
     this.init();
-    this.mongo();
   }
 
   init() {
@@ -25,13 +24,6 @@ class Database {
       // fazer a chamada do metodo associate(relacionamentos) somente nos models
       // que estiverem com o mesmo declarado
       .map(model => model.associate && model.associate(this.connection.models));
-  }
-
-  mongo() {
-    this.mongConnection = mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useFindAndModify: true
-    });
   }
 }
 
--- src/database/migrations/20191226031617-create-help-orders.js
+++ src/database/migrations/20191226031617-create-help-orders.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable("enrollments", {
+    return queryInterface.createTable("helpers", {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -38,6 +38,6 @@ module.exports = {
   },
 
   down: queryInterface => {
-    return queryInterface.dropTable("enrollments");
+    return queryInterface.dropTable("helpers");
   }
 };
